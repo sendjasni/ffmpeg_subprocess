6a27f1b2b0af0bff111ba470cb040f11c7d0da3d
AbderreZzaQ <sendjasni.abderrezzaq@gmail.com>
2019-03-05 17:04:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,18 +4,18 @@ import os
 
 
 FFMPEG_BIN = "C:/ffmpeg/bin/ffmpeg.exe"
-command = [ FFMPEG_BIN, '-y',
-            '-f', 'dshow', '-rtbufsize', '100M',
-            '-i', 'video=Datapath VisionAV Video 01' ,
-             '-video_size', '640x480',
-              '-pix_fmt', 'bgr24', '-r','25',  
-          '-f', 'image2pipe', '-' ]    
+command = [FFMPEG_BIN, '-y',
+           '-f', 'dshow', '-rtbufsize', '100M',
+           '-i', 'video=Datapath VisionAV Video 01',
+           '-video_size', '640x480',
+           '-pix_fmt', 'bgr24', '-r', '25',
+           '-f', 'image2pipe', '-']
 
 
-try:    
+try:
     pipe = sp.Popen(command, stdout=sp.PIPE, stderr=sp.STDOUT, universal_newlines=True)`
 
     ffmpeg_output, _ = pipe.communicate()
 
-except  sp.CalledProcessError as err:
-     print("FFmpeg stdout output on error:\n" + err.output)
+except sp.CalledProcessError as err:
+    print("FFmpeg stdout output on error:\n" + err.output)
